4f7ca9ed5feebb6471c3a3bde132ef294a5969c3
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2023-09-15 16:41:51 -0500

    feature: Finish design of the `header` with fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kickertrial: "KickerTrial-Black",
+        inputsans: "InputSans-ExtraLight",
+      }
+    },
   },
   plugins: [],
 }
