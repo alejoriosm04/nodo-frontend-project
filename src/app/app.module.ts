f2ebf9229c12bc69ed02e7cd86200ecfd939ee5e
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2023-09-16 15:06:23 -0500

    feature: Finish `main-slideshow` component and responsive design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainSlideshowComponent } from './main-slideshow/main-slideshow.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MainSlideshowComponent } from './main-slideshow/main-slideshow.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/main-slideshow/main-slideshow.component.ts
+++ src/app/main-slideshow/main-slideshow.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-main-slideshow',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-slideshow.component.css']
 })
 export class MainSlideshowComponent {
-
+  images = [
+    { name: 'todo-el-laboratorio-2.jpg' },
+    { name: 'image-1.jpg' },
+    { name: 'image-2.jpg' },
+    { name: 'percepcion-asombro.jpg' },
+    { name: 'sala-creacion.jpg' },
+    { name: 'sala-percepcion.jpg' },
+  ];
 }
