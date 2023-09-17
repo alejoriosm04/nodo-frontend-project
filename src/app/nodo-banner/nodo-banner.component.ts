723bfd0868280a96426fc3e08e0381a9ac76dc13
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2023-09-17 17:06:33 -0500

    feature: Design and create `nodo-banner` with styles and data

--- src/app/nodo-banner/nodo-banner.component.ts
+++ src/app/nodo-banner/nodo-banner.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./nodo-banner.component.css']
 })
 export class NodoBannerComponent {
-
+  images = [
+    { name: 'banner-fundamentals.jpg', link: 'https://www.nodoeafit.com/curso/1/fundamentals' },
+    { name: 'banner-frontend.jpg', link: 'https://www.nodoeafit.com/curso/2/frontend' },
+    { name: 'banner-backend.jpg', link: 'https://www.nodoeafit.com/curso/3/backend' },
+    { name: 'banner-cloud-native.jpg', link: 'https://www.nodoeafit.com/curso/2019/cloud-native' },
+    { name: 'banner-python.jpg', link: 'https://www.nodoeafit.com/curso/2026/python' },
+  ];
 }
