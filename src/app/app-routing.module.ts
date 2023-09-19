550d0d4586cb2e433aab856b90b947765687550c
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2023-09-19 12:26:15 -0500

    fix: Fix routing in `header` component and modify structure in `app.component`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
+import { LabSpacesComponent } from './lab-spaces/lab-spaces.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'form', component: FormComponent },
+  { path: 'labspaces', component: LabSpacesComponent },
 ];
 
 @NgModule({
